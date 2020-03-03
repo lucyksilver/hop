9b3391a71c6554cbce8db0cc33398448ce39cfe9
Vanda Adams <vandamadams@gmail.com>
2020-03-03 13:57:35 +0000

    added randomise to home page

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,4 +1,3 @@
-
 class BeersController < ApplicationController
 
   def index
@@ -6,9 +5,10 @@ class BeersController < ApplicationController
       @beers = Beer.search_by_name(params[:query])
     elsif params[:search]
       # includes acces the info of beers tags in the where
-
       tag_ids = params[:search][:tags].reject { |id| id == ""}
       @beers = Beer.includes(:beer_tags).where(beer_tags: { tag_id: tag_ids })
+    elsif
+      @beers = Beer.all.sample(3)
     else
       @beers = Beer.all
     end
