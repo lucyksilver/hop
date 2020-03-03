6c0c061243802096e6755159c9d4d281d78e9443
Vanda Adams <vandamadams@gmail.com>
2020-03-03 14:43:41 +0000

    updated search bar

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,16 +1,19 @@
 class BeersController < ApplicationController
 
   def index
+    # raise
     if params[:query]
-      @beers = Beer.search_by_name(params[:query])
+      if params[:query].strip.empty?
+        @beers = Beer.all
+      else
+        @beers = Beer.search_by_name(params[:query])
+      end
     elsif params[:search]
       # includes acces the info of beers tags in the where
       tag_ids = params[:search][:tags].reject { |id| id == ""}
       @beers = Beer.includes(:beer_tags).where(beer_tags: { tag_id: tag_ids })
-    elsif
-      @beers = Beer.all.sample(3)
     else
-      @beers = Beer.all
+      @beers = Beer.all.sample(3)
     end
   end
 
