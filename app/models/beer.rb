834beeba8b4cf93800f608d3f0bf74f03dd3be24
Rahul Robertson <rahulrobertson@hotmail.com>
2020-03-03 12:42:42 +0000

    made v1 user show page

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,6 @@
 class Beer < ApplicationRecord
   has_many :choices
-  has_many :beer_tag
-  has_many :pub_beer
+  has_many :beer_tags
+  has_many :pub_beers
+  has_many :pubs, through: :pub_beers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :choices
+  has_many :pubs, through: :choices
+  has_many :beers, through: :choices
 end
