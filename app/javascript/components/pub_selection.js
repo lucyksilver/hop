1336fa7e521bab3390f0af711456f3c22bd90121
Lucy Silver <lucy.silver2@gmail.com>
2020-03-04 13:49:56 +0000

    pub form

--- app/javascript/components/pub_selection.js
+++ app/javascript/components/pub_selection.js
@@ -1,5 +1,29 @@
-const myFunction = () => {
-  console.log("Hey there!")
+const pubSelection = () => {
+  const pubs = document.querySelectorAll(".card-home .form-check-label");
+  pubs.forEach((pub) => {
+    pub.addEventListener("click", (event) => {
+      // event.preventDefault();
+      console.log(event.currentTarget);
+      console.log(event.currentTarget.classList.contains('card-border'));
+      const otherCard = document.querySelector(".card-border");
+      if (otherCard) {
+
+        otherCard.classList.remove("card-border");
+      }
+      event.currentTarget.parentNode.parentNode.classList.toggle('card-border');
+    });
+  });
 };
 
-export { myFunction }
+
+
+// const cardBorder = document.getElementById("card-border") => {
+//   cardBorder.addEventListener("click", (event) => {
+//     event.currentTarget.classList.toggle("card-border");
+//   });
+// };
+
+
+
+export { pubSelection }
+
