901debb7490c00c22d2dc228bbe1f531c625169a
Rahul Robertson <rahulrobertson@hotmail.com>
2020-03-05 16:02:52 +0000

    v2 view for user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = current_user
+
+    @first_two = @user.choices.first(2)
+    if @user.choices.count > 2
+      @remainder = @user.choices.last(@user.choices.count - 2)
+    end
+
+    @first_two_likes = @user.likes.first(2)
+    if @user.likes.count > 2
+      @remainder_likes = @user.likes.last(@user.likes.count - 2)
+    end
   end
 end
