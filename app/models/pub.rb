fdbc3c5ccf28eeaba2bdb88282a585551696f9cb
Lucy Silver <lucy.silver2@gmail.com>
2020-03-06 16:12:18 +0000

    seeds

--- app/models/pub.rb
+++ app/models/pub.rb
@@ -4,6 +4,7 @@ class Pub < ApplicationRecord
   has_many :users, through: :choices
   has_many :pub_beers, dependent: :destroy
   has_many :beers, through: :pub_beers
+  has_one_attached :photo
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
