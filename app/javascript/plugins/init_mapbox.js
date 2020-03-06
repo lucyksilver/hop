56f30d7183f3099b12998ce190b81174b50032b4
Rahul Robertson <rahulrobertson@hotmail.com>
2020-03-06 15:49:06 +0000

    added user avatar and clear beer png to map, also added new btn class to login btn on homepage

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -19,6 +19,7 @@ const addMarkersToMap = (map, markers) => {
   const element = document.createElement('div');
   element.className = 'marker';
   element.style.backgroundImage = `url('${marker.image_url}')`;
+  element.style.backgroundRepeat = 'no-repeat';
   element.style.backgroundSize = 'contain';
   element.style.width = '25px';
   element.style.height = '25px';
