05c363abad7fd4e478cbeb118ccd62d19b12f3ee
Lucy Silver <lucy.silver2@gmail.com>
2020-03-12 09:54:25 +0000

    js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 import { initMapbox } from '../plugins/init_mapbox';
 import { pubSelection } from '../components/pub_selection';
 import { tagSelection } from '../components/tag_selection';
+import { randomizer } from '../components/randomizer';
 import ScrollReveal from 'scrollreveal'
 
 document.addEventListener('turbolinks:load', () => {
@@ -62,27 +63,27 @@ document.addEventListener('turbolinks:load', () => {
 
 import "bootstrap";
 
-const randomizer = () => {
-  let counter = 0;
-  const elem = document.getElementById("randomBeer");
-  const beers = JSON.parse(elem.dataset.beers);
-  const card = document.getElementById('card')
-  console.log(beers)
-  const actual = elem.dataset.actualBeer;
-  beers.push(actual);
-  const change = () => {
-    if (counter < beers.length) {
-      elem.innerHTML = beers[counter];
-      counter++;
-    } else if (counter === beers.length) {
-      clearInterval(change);
-      console.log(actual);
-      card.classList.remove('d-none');
-      elem.classList.remove('beer-title');
-    }
+// const randomizer = () => {
+//   let counter = 0;
+//   const elem = document.getElementById("randomBeer");
+//   const beers = JSON.parse(elem.dataset.beers);
+//   const card = document.getElementById('card')
+//   console.log(beers)
+//   const actual = elem.dataset.actualBeer;
+//   beers.push(actual);
+//   const change = () => {
+//     if (counter < beers.length) {
+//       elem.innerHTML = beers[counter];
+//       counter++;
+//     } else if (counter === beers.length) {
+//       clearInterval(change);
+//       console.log(actual);
+//       card.classList.remove('d-none');
+//       elem.classList.remove('beer-title');
+//     }
 
-  }
-  setInterval(change, 200);
-}
+//   }
+//   setInterval(change, 200);
+// }
 
 
